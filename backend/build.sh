44796203edb2c19ae7f886ab0ca786d07524058b
lazakichelsoube-prog <tresorchelsoube@gmail.com>
2026-08-17 11:02:47 +0000

    Ajout reset mdp admin au build

--- backend/build.sh
+++ backend/build.sh
@@ -2,3 +2,13 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from accounts.models import User
+if not User.objects.filter(username='admin@example.com').exists():
+    User.objects.create_superuser('admin@example.com', 'admin@example.com', 'test1234')
+else:
+    u = User.objects.get(username='admin@example.com')
+    u.set_password('test1234')
+    u.save()
+"
