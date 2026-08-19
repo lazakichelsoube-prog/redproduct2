bda63c41267970e7fad5ccd5ec83277d42ff2109
lazakichelsoube-prog <tresorchelsoube@gmail.com>
2026-08-19 16:00:13 +0000

    Clean Hotel model and update settings for Cloudinary

--- backend/hotels/models.py
+++ backend/hotels/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from cloudinary_storage.storage import MediaCloudinaryStorage
 
 class Hotel(models.Model):
     nom = models.CharField(max_length=255)
@@ -8,7 +7,7 @@ class Hotel(models.Model):
     telephone = models.CharField(max_length=20)
     prix_nuit = models.DecimalField(max_digits=10, decimal_places=2)
     devise = models.CharField(max_length=10, default='XOF')
-    photo = models.ImageField(upload_to='hotels/', storage=MediaCloudinaryStorage(), blank=True, null=True)
+    photo = models.ImageField(upload_to='hotels/', blank=True, null=True)
     date_creation = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
