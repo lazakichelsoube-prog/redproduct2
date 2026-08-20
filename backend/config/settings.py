1ddafc382a7b466da41b7cf2da0db09fafa280d2
lazakichelsoube-prog <tresorchelsoube@gmail.com>
2026-08-20 10:10:18 +0000

    Fix collectstatic error by using standard staticfiles storage

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,30 +6,24 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import dj_database_url
-
 try:
     from decouple import config
 except ImportError:
     config = os.getenv
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = config(
     'SECRET_KEY',
     default='django-insecure-0m35r#q@6@((37w6x+z(spye)ts%he1=#_&!qhw$p2w$mb5$g9'
 )
-
 DEBUG = config('DEBUG', default=True, cast=bool)
-
 ALLOWED_HOSTS = ['*']
-
 INSTALLED_APPS = [
-    'cloudinary_storage',  # OBLIGATOIRE : Doit être placé avant django.contrib.staticfiles et admin
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
@@ -37,7 +31,6 @@ INSTALLED_APPS = [
     'accounts',
     'hotels',
 ]
-
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -49,9 +42,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'config.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -66,61 +57,54 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'config.wsgi.application'
-
 DATABASES = {
     'default': dj_database_url.config(
         default='postgresql://postgres:postgres123@localhost:5432/redproductdatabase',
         conn_max_age=600
     )
 }
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME', default=''),
     'API_KEY': config('CLOUDINARY_API_KEY', default=''),
     'API_SECRET': config('CLOUDINARY_API_SECRET', default=''),
 }
-
-# Compatibilité double pour imposer le stockage Cloudinary
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 STORAGES = {
     'default': {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
     },
 }
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
+# Compatibilité avec django-cloudinary-storage qui utilise encore
+# l'ancien nom de réglage STATICFILES_STORAGE.
+# On utilise le storage Django standard (sans compression au build)
+# pour éviter un bug connu entre django-cloudinary-storage et whitenoise
+# qui fait planter collectstatic sur des fichiers manquants
+# (ex: cloudinary/js/jquery.fileupload-process.js).
+# Le middleware WhiteNoiseMiddleware (déjà actif) continue de servir
+# et compresser les fichiers statiques à la volée en production.
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 CORS_ALLOW_ALL_ORIGINS = True
 AUTH_USER_MODEL = 'accounts.User'
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
